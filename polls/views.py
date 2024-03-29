9f350dfd19e2abc577b2d633bd19f79b104a8664
implaymo <goncalomorerasilva@gmail.com>
2024-03-29 17:14:30 +0000

    Delete databases that were empty. Manage to show name of cafe and location.

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,5 @@ from .models import Cafe
 
 
 def index(request): 
-    return render(request, 'index.html')
-
-def get_all_cafes(request):
-    all_map_urls = Cafe.objects.values_list('map_url', flat=True)
-    return render(request, 'cafe_list.html', {'all_map_urls': all_map_urls})
+    all_cafes = Cafe.objects.all()
+    return render(request, 'index.html', {'all_cafes': all_cafes})

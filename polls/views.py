53d5dae5c9a4f38fda944fd1b0e32a49995acbd0
implaymo <136201112+implaymo@users.noreply.github.com>
2024-03-29 16:19:49 +0000

    Manage to get the data i want from database.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Cafe
 
 
 def index(request): 
     return render(request, 'index.html')
+
+def get_all_cafes(request):
+    all_map_urls = Cafe.objects.values_list('map_url', flat=True)
+    return render(request, 'cafe_list.html', {'all_map_urls': all_map_urls})

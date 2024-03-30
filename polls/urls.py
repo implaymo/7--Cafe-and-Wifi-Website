58cdb5f761a960bfd11a14c241ecae5a06505f7a
implaymo <goncalomorerasilva@gmail.com>
2024-03-30 12:11:18 +0000

    Manage to show the cafe info at Heroes if user clicks cafe on the list

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('get_cafe_info/', views.get_cafe_info, name='get_cafe_info'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import Cafe
 
 
 def index(request): 
     all_cafes = Cafe.objects.all()
     return render(request, 'index.html', {'all_cafes': all_cafes})
+
+def get_cafe_info(request):
+    all_cafes = Cafe.objects.all()
+    cafe_id = request.GET.get('cafe_id')
+    cafe = get_object_or_404(Cafe, id=cafe_id)
+    return render(request, 'index.html', {'all_cafes': all_cafes, 'cafe': cafe})
